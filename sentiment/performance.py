fb4b56ae971d2ee834be152de9846ade824262bb
Sagar <znerfii.a1@gmail.com>
2024-10-15 16:14:56 -0500

    feat: sentiment LT CT

--- sentiment/performance.py
+++ sentiment/performance.py
@@ -13,101 +13,111 @@ def measure_time(func):
     return wrapper
 
 @measure_time
-def run_hadoop_job(jar_file, input_text):
+def run_hadoop_job(jar_file, input_file):
+    hdfs_input_path = "/tmp/temp_input_file.txt"  
+    hdfs_output_dir = "/tmp/hadoop_output" 
+
     try:
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.txt') as temp_input_file:
-            upload_start = time.time()
-            temp_input_file.write(input_text.encode('utf-8'))
-            temp_input_file.close()
+        upload_start = time.time()
 
-            hdfs_input_path = "/tmp/temp_input_file.txt"  
-            hdfs_output_dir = "/tmp/hadoop_output"         
-
-            upload_cmd = ["hdfs", "dfs", "-put", temp_input_file.name, hdfs_input_path]
-            upload_process = subprocess.Popen(upload_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            upload_stdout, upload_stderr = upload_process.communicate()
+        upload_cmd = ["hdfs", "dfs", "-put", input_file, hdfs_input_path]
+        upload_process = subprocess.Popen(upload_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        upload_stdout, upload_stderr = upload_process.communicate()
 
 
-            if upload_process.returncode != 0:
-                print("Failed to upload file to HDFS.")
-                print("Errors:\n", upload_stderr.decode())
-                return
-            upload_end = time.time()
-            print("Upload time::", upload_end-upload_start)
-            
+        if upload_process.returncode != 0:
+            print("Failed to upload file to HDFS.")
+            print("Errors:\n", upload_stderr.decode())
+            return
+        upload_end = time.time()
+        print("Upload time::", upload_end-upload_start)
+        
 
-            hadoop_cmd = [
-                'hadoop', 'jar', jar_file, 'Sentiment',
-                hdfs_input_path,
-                hdfs_output_dir
-            ]
+        hadoop_cmd = [
+            'hadoop', 'jar', jar_file, 'Sentiment',
+            hdfs_input_path,
+            hdfs_output_dir
+        ]
 
-            process_start = time.time()
-            process = subprocess.Popen(hadoop_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = process.communicate()
-            process_end = time.time()
+        process_start = time.time()
+        process = subprocess.Popen(hadoop_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        process_end = time.time()
 
-            print("Mapreduce Job time::", process_end-process_start)
+        print("Mapreduce Job time::", process_end-process_start)
 
-            clearing_start = time.time()
-            if process.returncode == 0:
-                print("Hadoop job completed successfully.")
+        clearing_start = time.time()
+        if process.returncode == 0:
+            print("Hadoop job completed successfully.")
 
-                output_files_cmd = ["hdfs", "dfs", "-ls", hdfs_output_dir]
-                output_files_process = subprocess.Popen(output_files_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                output_files_stdout, output_files_stderr = output_files_process.communicate()
+            output_files_cmd = ["hdfs", "dfs", "-ls", hdfs_output_dir]
+            output_files_process = subprocess.Popen(output_files_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output_files_stdout, output_files_stderr = output_files_process.communicate()
 
-                if output_files_process.returncode == 0:
-
-                    output_files = [line.split()[-1] for line in output_files_stdout.decode().splitlines() if line]
-                    for output_file in output_files:
-                        print("Output from {}:".format(output_file))
-                        
-                        cat_cmd = ["hdfs", "dfs", "-cat", output_file]
-                        cat_process = subprocess.Popen(cat_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                        cat_stdout, cat_stderr = cat_process.communicate()
-                        print(cat_stdout.decode())
-                else:
-                    print("Failed to list output files.")
-                    print("Errors:\n", output_files_stderr.decode())
+            if output_files_process.returncode == 0:
+                print("Success")
             else:
-                print("Hadoop job failed with return code:", process.returncode)
-                print("Errors:\n", stderr.decode())
+                print("Failed to list output files.")
+                print("Errors:\n", output_files_stderr.decode())
+        else:
+            print("Hadoop job failed with return code:", process.returncode)
+            print("Errors:\n", stderr.decode())
+        
+        cleanup_input_cmd = ["hdfs", "dfs", "-rm", hdfs_input_path]
+        
+        cleanup_output_cmd = ["hdfs", "dfs", "-rm", "-r", hdfs_output_dir]
+        
+        cleanup_output_process = subprocess.Popen(cleanup_output_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cleanup_output_stdout, cleanup_output_stderr = cleanup_output_process.communicate()
 
-            
-            os.remove(temp_input_file.name)
+        if cleanup_output_process.returncode == 0:
+            print("Successfully removed output directory from HDFS:",hdfs_output_dir)
+        else:
+            print("Failed to remove output directory from HDFS.")
+            print("Errors:\n", cleanup_output_stderr.decode())
 
-            
-            cleanup_input_cmd = ["hdfs", "dfs", "-rm", hdfs_input_path]
-            
-            cleanup_output_cmd = ["hdfs", "dfs", "-rm", "-r", hdfs_output_dir]
-            
-            cleanup_output_process = subprocess.Popen(cleanup_output_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            cleanup_output_stdout, cleanup_output_stderr = cleanup_output_process.communicate()
+        cleanup_output_process = subprocess.Popen(cleanup_input_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cleanup_output_stdout, cleanup_output_stderr = cleanup_output_process.communicate()
 
-            if cleanup_output_process.returncode == 0:
-                print("Successfully removed output directory from HDFS:",hdfs_output_dir)
-            else:
-                print("Failed to remove output directory from HDFS.")
-                print("Errors:\n", cleanup_output_stderr.decode())
+        if cleanup_output_process.returncode == 0:
+            print("Successfully removed output directory from HDFS:",hdfs_output_dir)
+        else:
+            print("Failed to remove output directory from HDFS.")
+            print("Errors:\n", cleanup_output_stderr.decode())
+        clearing_end = time.time()
 
-            cleanup_output_process = subprocess.Popen(cleanup_input_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            cleanup_output_stdout, cleanup_output_stderr = cleanup_output_process.communicate()
-
-            if cleanup_output_process.returncode == 0:
-                print("Successfully removed output directory from HDFS:",hdfs_output_dir)
-            else:
-                print("Failed to remove output directory from HDFS.")
-                print("Errors:\n", cleanup_output_stderr.decode())
-            clearing_end = time.time()
-
-            print("Clearing time::", clearing_end-clearing_start)
+        print("Clearing time::", clearing_end-clearing_start)
     except Exception as e:
         print("An error occurred:", str(e))
+
+        cleanup_input_cmd = ["hdfs", "dfs", "-rm", hdfs_input_path]
+        
+        cleanup_output_cmd = ["hdfs", "dfs", "-rm", "-r", hdfs_output_dir]
+        
+        cleanup_output_process = subprocess.Popen(cleanup_output_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cleanup_output_stdout, cleanup_output_stderr = cleanup_output_process.communicate()
+
+        if cleanup_output_process.returncode == 0:
+            print("Successfully removed output directory from HDFS:",hdfs_output_dir)
+        else:
+            print("Failed to remove output directory from HDFS.")
+            print("Errors:\n", cleanup_output_stderr.decode())
+
+        cleanup_output_process = subprocess.Popen(cleanup_input_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cleanup_output_stdout, cleanup_output_stderr = cleanup_output_process.communicate()
+
+        if cleanup_output_process.returncode == 0:
+            print("Successfully removed output directory from HDFS:",hdfs_output_dir)
+        else:
+            print("Failed to remove output directory from HDFS.")
+            print("Errors:\n", cleanup_output_stderr.decode())
 
 
 # main
 jar_file = "sentiment/sentiment.jar"
-input_text = "Hello world Hello Hadoop\nThis is a simple test\nHello again"
-run_hadoop_job(jar_file, input_text)
+
+for x in ["4.txt","8.txt","12.txt","16.txt"]:
+    print("Executing file ::",x)
+    input_text = "txt/"+x
+    run_hadoop_job(jar_file, input_text)
 
